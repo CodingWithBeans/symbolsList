8e26bdedb0b7a086a66b3cacb44652c1a15dcbc4
beans <mattylee0121@gmail.com>
2025-10-23 19:31:02 +0100

    bug with installing dependancies, easiest fix is to just make them seperate scripts

--- symbolslistscripts.py
+++ symbolslistscripts.py
@@ -8,47 +8,10 @@
 # will then need to go through document and check for things like GCD
 
 #installing dependencies
-import subprocess, sys, time, itertools
 from os import system
 import tkinter as tk
 from tkinter import filedialog as fd
-
-#packages should be entered in string format. # e.g "PyMuPDF"
-packages = {"PyMuPDF":"fitz"}
-
-#quick fake spinner
-spinner = itertools.cycle("▖▘▝▗")
-
-def permission(packages):
-
-    answer = input(f"The Script will now attempt to install the following libraries {packages}! Do you wish to continue? [Y/N]").strip().upper()
-    system('cls')
-    match answer:
-        case "N":
-            sys.exit()
-        case "Y":
-            return 0
-        case _:
-            print("Please answer either Y or N")
-            return permission(packages)
-    
-
-def installDependencies(packages, spinner):
-
-    for pipCommand, importCommand in packages.items():
-        try:
-            print(f"testing import {pipCommand}...", flush=True)
-            #i could while loops this but will require multi threads and honestly faking it is enough.
-            for _ in range(5):
-                time.sleep(0.5)
-                print(f"\rImporting {pipCommand} {next(spinner)}", end="", flush=True)
-            __import__(importCommand)
-        except ImportError:
-            print(f"\nInstalling required package: {pipCommand} please standby...")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", pipCommand])
-    system('cls')
-    print("Packages installed.")
-
+from collections import defaultdict, Counter
 import fitz
 
 def grabFilePath():
@@ -70,8 +33,8 @@ def openPDF(path):
 
 def parsePDF(pdf):
     """Parse the PDF and count symbols."""
-    symbolCount = {}
-    symbolPage = {}
+    symbolCount = Counter()
+    symbolPage = defaultdict(set)
 
     for pageNumber in range(1, len(pdf) + 1):
         page = pdf.load_page(pageNumber - 1)
@@ -98,10 +61,6 @@ def parsePDF(pdf):
 
 def main():
 
-    system('cls')
-    permission(packages)
-    system('cls')
-    installDependencies(packages, spinner)
     filePath = grabFilePath()
     pdf = openPDF(filePath)
     parsePDF(pdf)
